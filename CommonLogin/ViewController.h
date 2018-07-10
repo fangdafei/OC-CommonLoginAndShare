efbb4862e473f6a392b94ad3c9ca477efbae0086
fangrifei <fangrifei@100tal.com>
2018-07-10 11:27:29 +0800

    Initial Commit

---
+++ CommonLogin/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CommonLogin
+//
+//  Created by xrs_fang on 2018/7/10.
+//  Copyright © 2018年 xrs_fang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ CommonLogin/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  CommonLogin
+//
+//  Created by xrs_fang on 2018/7/10.
+//  Copyright © 2018年 xrs_fang. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
